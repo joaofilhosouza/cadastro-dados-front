0a51f8cef88c191d6042d2cadcf3a300207fb221
jfilinho <filinhosouza10@gmail.com>
2022-04-06 15:22:02 -0300

    arquivo Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ListaUsuario from "./ListaTabela";
+import ListaUsuario from "./TabelaUsuario";
 import { HomeContainer, DivHome, DivTh } from "./Tabela.style";
 
 function Home() {
