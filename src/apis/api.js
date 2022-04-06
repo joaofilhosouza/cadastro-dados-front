77f12b3448971fb956aa2d7f7e430df6a09295c7
jfilinho <filinhosouza10@gmail.com>
2022-04-06 13:52:07 -0300

    retirando headers de api

--- src/apis/api.js
+++ src/apis/api.js
@@ -10,20 +10,4 @@ const api = axios.create({
   baseURL: apis[process.env.NODE_ENV],
 });
 
-// Configura a instância do Axios para injetar o cabeçalho de autenticação antes de cada requisição
-api.interceptors.request.use((config) => {
-  // Verifica se já temos as informações do usuário logado no localStorage
-  const storedUser = localStorage.getItem("loggedInUser");
-
-  const loggedInUser = JSON.parse(storedUser || '""');
-
-  if (loggedInUser.token) {
-    config.headers = {
-      Authorization: `Bearer ${loggedInUser.token}`,
-    };
-  }
-
-  return config;
-});
-
 export default api;
